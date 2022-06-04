f9240b5effbaa0637490c17b0b5d16ff8d23c95d
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-06-04 15:59:33 +0530

    Rendered my first react component.

--- application/src/index.js
+++ application/src/index.js
@@ -1,2 +1,9 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 
+function Greeting() {
+  return <h1>This is my first react component.</h1>
+}
+
+
+ReactDom.render(<Greeting />, document.getElementById('root'))

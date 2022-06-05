3f79a40c347b231595421094802c054a343f7073
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-06-05 09:50:30 +0530

    List Check

--- application/src/index.js
+++ application/src/index.js
@@ -7,6 +7,12 @@ function Greeting() {
   return (
     <div>
       <h1>Hello World</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
     </div>
   );
 }

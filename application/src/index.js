5528f8c7ef73844478d793fdf769d750c7033171
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-06-06 15:21:31 +0530

    Understood props and Also Boook list is complete.

--- application/src/index.js
+++ application/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import base from "./css/base.css";
+import title from "./images/title.jpg";
+import { useState } from "react";
 
 // function Greeting() {
 //   return (
@@ -42,16 +44,60 @@ import base from "./css/base.css";
 //   return <h3>Did someone summon third Greeting.</h3>;
 // }
 
+// Data Test. Following is a mock of data which can be recieved through API.
+var titles = [
+  "Ikigai: The Japanese Secret to a Long and Happy Life",
+  "The Psychology of Money: Timeless lessons on wealth, greed, and happiness",
+  "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+  "The 48 Laws of Power",
+  "The Art of War",
+  "How to Win Friends & Influence People",
+];
+var authors = [
+  "Héctor García, Francesc Miralles",
+  "Morgan Housel",
+  "James Clear",
+  "Robert Greene",
+  "Sun Tzu",
+  "Dale Carnegie",
+];
+
+var imglink = [
+  "https://images-na.ssl-images-amazon.com/images/I/41SG4SO4T9L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/41y1sBiezmL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/51-uspgqWIL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/41KY-NORo9L._SX355_BO1,204,203,200_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/41mUhMo3qvL._SX322_BO1,204,203,200_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/41C9YOYkkOL._SX320_BO1,204,203,200_.jpg",
+];
+
 function BookList() {
+  const list = [];
+  var [val, setval] = useState(0);
+  for (var index = 0; index < titles.length; index++) {
+    list.push(<Book i={index} />);
+  }
   return (
-    <section>
-      <Book />
+    <section className="list">
+      {list}
+      <button onClick={() => setval((val += 1))}>Increment</button>
+      <p>The value is: {val}</p>
     </section>
   );
 }
 
-function Book() {
-  return <article>This is a book.</article>;
+function Book(props) {
+  return (
+    <article className="book-card">
+      <Image i={props.i} />
+      <Title i={props.i} />
+      <Author i={props.i} />
+    </article>
+  );
 }
+
+const Image = (props) => <img src={imglink[props.i]} alt="" height="300px" />;
+const Title = (props) => <h2 className="title">{titles[props.i]}</h2>;
+const Author = (props) => <h1 className="author">{authors[props.i]}</h1>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));

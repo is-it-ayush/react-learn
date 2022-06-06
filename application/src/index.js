f7efb67ccf0ab866ca4999ac3a6e8ffde6ff5a0e
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-06-06 15:40:02 +0530

    CleanedCode-AddedComments-LayoutChanged

--- application/src/index.js
+++ application/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 import base from "./css/base.css";
-import title from "./images/title.jpg";
 import { useState } from "react";
 
 // function Greeting() {
@@ -44,7 +43,7 @@ import { useState } from "react";
 //   return <h3>Did someone summon third Greeting.</h3>;
 // }
 
-// Data Test. Following is a mock of data which can be recieved through API.
+// Data Test. Following is a mock of data which can be fetched through API.
 var titles = [
   "Ikigai: The Japanese Secret to a Long and Happy Life",
   "The Psychology of Money: Timeless lessons on wealth, greed, and happiness",
@@ -72,23 +71,34 @@ var imglink = [
 ];
 
 function BookList() {
+  //This stores the book list which is rendered.
   const list = [];
+
+  //This is a test for using statefulness of React Components.
   var [val, setval] = useState(0);
+
+  //To fill up the above list[] with Books and their prop i
   for (var index = 0; index < titles.length; index++) {
     list.push(<Book i={index} />);
   }
+
+  // Return the entire app.
   return (
     <section className="list">
-      {list}
+      {/** Button updates the val defined above as a updateable var. */}
       <button onClick={() => setval((val += 1))}>Increment</button>
       <p>The value is: {val}</p>
+
+      {list}
     </section>
   );
 }
 
 function Book(props) {
+  //Takes up props.
   return (
     <article className="book-card">
+      {/** Since the lists are already sorted (representing mock data in order). I can directly assign prop values to each without worry. */}
       <Image i={props.i} />
       <Title i={props.i} />
       <Author i={props.i} />
@@ -96,8 +106,10 @@ function Book(props) {
   );
 }
 
+// The other components, they take up props and finally we get each specific component.
 const Image = (props) => <img src={imglink[props.i]} alt="" height="300px" />;
 const Title = (props) => <h2 className="title">{titles[props.i]}</h2>;
 const Author = (props) => <h1 className="author">{authors[props.i]}</h1>;
 
+//Important: This renders our BookList Component.
 ReactDom.render(<BookList />, document.getElementById("root"));

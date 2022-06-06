6365b4a7424e2f4c4e4caba2b7dc70be8f0104fd
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-06-06 13:16:04 +0530

    Incorporated CSS and also multi-components.

--- application/src/index.js
+++ application/src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-let i = 10;
+import base from "./css/base.css";
 
 function Greeting() {
+  var i = 0;
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="test">Hello World {i}</h1>
       <ul>
         <li>Item 1</li>
         <li>Item 2</li>
         <li>Item 3</li>
         <li>Item 4</li>
+        <li onClick={(i += 1)}>
+          <a href="#">This is an example link test. </a>
+        </li>
       </ul>
+      <div>
+        <SecondGreeting />
+      </div>
+    </div>
+  );
+}
+
+function SecondGreeting() {
+  return (
+    <div>
+      <h3>This is rendered via SecondGreeting().</h3>
     </div>
   );
 }

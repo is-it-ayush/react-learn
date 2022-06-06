a53eb2cdfcfb764350f0b0d20d2e65da0d445c56
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-06-06 14:03:05 +0530

    Commented out previous code-To Create a simple book list project.

--- application/src/index.js
+++ application/src/index.js
@@ -2,44 +2,56 @@ import React from "react";
 import ReactDom from "react-dom";
 import base from "./css/base.css";
 
-function Greeting() {
-  return (
-    <div>
-      <h1 className="test">Hello World</h1>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-        <li>
-          <a href="#">This is an example link test. </a>
-        </li>
-      </ul>
-      <div>
-        <SecondGreeting/>
-        <ThirdGreeting />
+// function Greeting() {
+//   return (
+//     <div>
+//       <h1 className="test">Hello World</h1>
+//       <ul>
+//         <li>Item 1</li>
+//         <li>Item 2</li>
+//         <li>Item 3</li>
+//         <li>Item 4</li>
+//         <li>
+//           <a href="#">This is an example link test. </a>
+//         </li>
+//       </ul>
+//       <div>
+//         <SecondGreeting />
+//         <ThirdGreeting />
 
-        {/* This is a comment test. */}
-        {/**
-         * This is a multi-line comment test.
-         * The line below is a re-use of a component.
-         */}
-        <SecondGreeting />
-      </div>
-    </div>
+//         {/* This is a comment test. */}
+//         {/**
+//          * This is a multi-line comment test.
+//          * The line below is a re-use of a component.
+//          */}
+//         <SecondGreeting />
+//       </div>
+//     </div>
+//   );
+// }
+
+// function SecondGreeting() {
+//   return (
+//     <div>
+//       <h3>This is rendered via SecondGreeting().</h3>
+//     </div>
+//   );
+// }
+
+// function ThirdGreeting() {
+//   return <h3>Did someone summon third Greeting.</h3>;
+// }
+
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
   );
 }
 
-function SecondGreeting() {
-  return (
-    <div>
-      <h3>This is rendered via SecondGreeting().</h3>
-    </div>
-  );
+function Book() {
+  return <article>This is a book.</article>;
 }
 
-function ThirdGreeting() {
-  return <h3>Did someone summon third Greeting.</h3>;
-}
-
-ReactDom.render(<Greeting />, document.getElementById("root"));
+ReactDom.render(<BookList />, document.getElementById("root"));
